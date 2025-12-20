fae6872f098de82108c45cf72de50a5d44781f89
Ryan Furrer <ryandotfurrer@gmail.com>
2025-12-20 17:00:40 -0500

    add sanity and tailwind typography plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,23 @@
 // @ts-check
-import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel";
 import sitemap from "@astrojs/sitemap";
-
+import vercel from "@astrojs/vercel";
+import sanity from "@sanity/astro";
 import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   adapter: vercel(),
-  integrations: [react(), sitemap()],
-
+  integrations: [
+    react(),
+    sitemap(),
+    sanity({
+      projectId: "z2j0j9ei",
+      dataset: "production",
+      useCdn: false,
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
