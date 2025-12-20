dffa7d16cc12371f2ea02da201ca1e6de6c7f6c6
Ryan Furrer <ryandotfurrer@gmail.com>
2025-12-20 15:43:06 -0500

    initial commit: added react, vercel adapater, astro sitemap, and tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import vercel from "@astrojs/vercel";
+import sitemap from "@astrojs/sitemap";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  adapter: vercel(),
+  integrations: [react(), sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
