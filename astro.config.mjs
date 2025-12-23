6d745452263604acac46f8fdb4fb8fcbabedb658
Ryan Furrer <ryandotfurrer@gmail.com>
2025-12-23 09:13:46 -0500

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,33 @@ import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel";
 import sanity from "@sanity/astro";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   adapter: vercel(),
+  experimental: {
+    fonts: [
+      {
+        name: "Geist",
+        cssVariable: "--font-geist",
+        provider: fontProviders.google(),
+        weights: [400, 500, 600, 700, 800],
+        styles: ["normal", "italic"],
+        subsets: ["latin", "cyrillic"],
+        fallbacks: ["sans-serif"],
+      },
+      {
+        name: "Geist Mono",
+        cssVariable: "--font-geist-mono",
+        provider: fontProviders.google(),
+        weights: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+        styles: ["normal", "italic"],
+        subsets: ["latin", "latin-ext"],
+        fallbacks: ["monospace"],
+      },
+    ],
+  },
   integrations: [
     react(),
     sitemap(),
