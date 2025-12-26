536e279571ddac46935fe0b6d00d4d36d347b00d
Ryan Furrer <ryandotfurrer@gmail.com>
2025-12-26 18:45:59 -0500

    add description to body

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,34 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export interface PortableTextBlock {
+  _type: "block";
+  _key: string;
+  style: string;
+  children: Array<{
+    _type: "span";
+    text: string;
+    marks: string[];
+  }>;
+  markDefs: unknown[];
+}
+
+export function textToPortableTextBlock(
+  text: string,
+  key: string = `description-${Date.now()}-${Math.random().toString(36).substring(7)}`,
+): PortableTextBlock {
+  return {
+    _type: "block",
+    _key: key,
+    style: "normal",
+    children: [
+      {
+        _type: "span",
+        text: text,
+        marks: [],
+      },
+    ],
+    markDefs: [],
+  };
+}
